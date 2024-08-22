5628707557adcff7e3c3121aa0ad02402cdb5ef2
Alice Blank <alicejblank@gmail.com>
2024-08-22 12:46:30 -0500

    feat(easy): implemented lengthOfLastWord

---
+++ easy/lengthOfLastWord.ts
@@ -0,0 +1,10 @@
+/*
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal
+substring consisting of non-space characters only.
+*/
+function lengthOfLastWord(s: string): number {
+  const splitS = s.split(' ')
+  return splitS[-1].length
+};
