3eac48be741752cccc7e24cf82667e1ad30577b8
Alice Blank <alicejblank@gmail.com>
2024-11-12 13:10:37 -0600

    feat(medium): buildings with an ocean view

---
+++ medium/buildingsWithAnOceanView.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} heights
+ * @return {number[]}
+ */
+var findBuildings = function(heights) {
+  let stack = [];
+  for (let i = 0; i < heights.length; i++) {
+      while (stack.length > 0 && heights[stack[stack.length - 1]] <= heights[i]) {
+          stack.pop();
+      }
+      stack.push(i);
+  }
+  return stack;
+};
